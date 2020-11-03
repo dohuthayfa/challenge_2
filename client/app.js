90e4064499f01502f8f115ab2954e4ca37f578d4
dohuthayfa <65371408+dohuthayfa@users.noreply.github.com>
2020-11-03 20:01:17 +0200

    bye bye challenge2

--- client/app.js
+++ client/app.js
@@ -17,3 +17,13 @@ $('#testButton').click(function () {
 });
 });
 
+// App 
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
