9cb620da7cea437fabca5cfb9e6385a924905323
dohuthayfa <65371408+dohuthayfa@users.noreply.github.com>
2020-11-03 14:38:24 +0200

    Ohhhh Noooo

--- client/app.js
+++ client/app.js
@@ -1,2 +1,16 @@
 
 
+//Submitting data with AJAX 
+$('#testButton').click(function () {
+    var firstName = $("#name").val();
+    var lastName = $("#LastName").val();
+
+    $.ajax({
+      type: "POST",
+      url: "insert.php",
+      data: { name: firstName, LastName: lastName }
+    }).done(function( msg ) {
+      alert( "Data Saved: " + msg );
+});
+});
+
